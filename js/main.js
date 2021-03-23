5bcb1955d9396eccceab072555ba5c453418ba25
etozhenerk <etozhenerk@yandex.ru>
2021-03-23 22:39:54 +0300

    Первый день

--- js/main.js
+++ js/main.js
@@ -1,10 +1,53 @@
-const mySwiper = new Swiper('.swiper-container', {
-	loop: true,
+"use strict";
 
-	// Navigation arrows
-	navigation: {
-		nextEl: '.slider-button-next',
-		prevEl: '.slider-button-prev',
-	},
+const mySwiper = new Swiper(".swiper-container", {
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".slider-button-next",
+    prevEl: ".slider-button-prev",
+  },
 });
 
+// cart
+
+const togglePopup = () => {
+  const buttonCart = document.querySelector(".button-cart"),
+    modalCart = document.querySelector("#modal-cart"),
+    modalClose = document.querySelector(".modal-close");
+
+  const openModal = () => {
+    modalCart.classList.add("show");
+  };
+  const closeModal = (e) => {
+    const target = e.target;
+    if (target === modalClose || target === modalCart) {
+      modalCart.classList.remove("show");
+    }
+  };
+
+  buttonCart.addEventListener("click", openModal);
+  modalCart.addEventListener("click", closeModal);
+};
+
+togglePopup();
+
+// scroll smoth
+
+const scrollSmoth = () => {
+  const scrollLinks = document.querySelectorAll("a.scroll-link");
+
+  scrollLinks.forEach((scrollLink) => {
+    scrollLink.addEventListener("click", (e) => {
+      e.preventDefault();
+      const id = scrollLink.getAttribute("href");
+      document.querySelector(id).scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+      });
+    });
+  });
+};
+
+scrollSmoth();
